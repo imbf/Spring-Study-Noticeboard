cb239e6b3f9583c986dcebe3f66141268ed21074
배종진 <qowhdwlsdk@gmail.com>
2020-04-02 18:17:29 +0900

    feat : 게시판 DTO 구현

---
+++ src/main/java/board/domain/BoardDTO.java
@@ -0,0 +1,30 @@
+package board.domain;
+
+import org.apache.ibatis.type.Alias;
+
+import java.sql.Timestamp;
+
+@Alias("boardDTO")
+public class BoardDTO {
+    private int seq;
+    private String title;
+    private String content;
+    private String writer;
+    private int password;
+    private Timestamp regDate;
+    private int cnt;
+
+    public BoardDTO() {
+    }
+
+    public BoardDTO(String title, String content, String writer, int password) {
+        super();
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.password = password;
+        this.cnt = 0;
+    }
+
+    // getter, setter 생략
+}
---
+++ src/main/java/board/domain/BoardDaoMyBatis.java
@@ -0,0 +1,54 @@
+package board.domain;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class BoardDaoMyBatis implements BoardDao{
+    private SqlSessionTemplate sqlSessionTemplate;
+
+    @Autowired // sqlSessionTemplate Bean주입 (Sql을 맵핑하는 클라이언트를 설정하는 Autowired Setter이다.)
+    public void setSqlMapClient(SqlSessionTemplate sqlSessionTemplate) {
+        this.sqlSessionTemplate = sqlSessionTemplate;
+    }
+
+    @Override
+    public List<BoardDTO> list() {
+        // 인자로써 SQL 스크립트 파일에 명시된 id를 준다.
+        return sqlSessionTemplate.selectList("list");
+    }
+
+    @Override
+    public int delete(BoardDTO boardDTO) {
+        // 인자로써 SQL 스크립트 파일에 명시된 id와 parameterType의 객체를 준다.
+        return sqlSessionTemplate.delete("delete", boardDTO);
+    }
+
+    @Override
+    public int deleteAll() {
+        return sqlSessionTemplate.delete("deleteAll");
+    }
+
+    @Override
+    public int update(BoardDTO boardDTO) {
+        return sqlSessionTemplate.update("update", boardDTO);
+    }
+
+    @Override
+    public int insert(BoardDTO boardDTO) {
+        return sqlSessionTemplate.insert("insert", boardDTO);
+    }
+
+    @Override
+    public BoardDTO select(int seq) {
+        return sqlSessionTemplate.selectOne("select", seq);
+    }
+
+    @Override
+    public int updateReadCount(int seq) {
+        return sqlSessionTemplate.update("updateCount", seq);
+    }
+}
